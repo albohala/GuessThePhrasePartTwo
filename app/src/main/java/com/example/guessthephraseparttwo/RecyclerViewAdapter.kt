8d566c819cc3a262762d8897f5f608fa3451a138
albohala <albohala@oregonstate.edu>
2021-12-04 18:20:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/guessthephraseparttwo/RecyclerViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.guessthephraseparttwo
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class RecyclerViewAdapter(val context: Context, val messages: ArrayList<String>): RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>() {
+    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = messages[position]
+
+        holder.itemView.apply {
+            tvItemsEntered.text = item
+            if(item.startsWith("Found")){
+                tvItemsEntered.setTextColor(Color.GREEN)
+            }else if(item.startsWith("No")||item.startsWith("Wrong")){
+                tvItemsEntered.setTextColor(Color.RED)
+            }else{
+                tvItemsEntered.setTextColor(Color.BLACK)
+            }
+        }
+    }
+
+    override fun getItemCount() = messages.size
+}
